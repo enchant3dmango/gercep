10c1238b7cb4dc665b5219e4e427fede8716f62a
enchant3dmango <andresitorusz.19@gmail.com>
2024-10-29 12:58:55 +0700

    feat: add gke_node_sa_roles var in gke_node_sa module

--- erflo-on-jikei/modules/gke_node_sa/main.tf
+++ erflo-on-jikei/modules/gke_node_sa/main.tf
@@ -3,7 +3,7 @@ resource "random_id" "cluster_suffix" {
   byte_length = 4
 }
 
-# Create GKE Node Service Account
+# Create GKE node service account
 resource "google_service_account" "gke_node_sa" {
   account_id   = "gke-${var.cluster_name}-${random_id.cluster_suffix.hex}"
   display_name = "GKE node service account for ${var.cluster_name}"
@@ -12,19 +12,14 @@ resource "google_service_account" "gke_node_sa" {
 
 # Assign required roles to the service account for project-level permissions
 resource "google_project_iam_member" "gke_node_sa_roles" {
-  for_each = toset([
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
-    "roles/monitoring.viewer",
-    "roles/stackdriver.resourceMetadata.writer"
-  ])
+  for_each = toset(var.gke_node_sa_roles)
 
   project = var.project_id
   role    = each.key
   member  = "serviceAccount:${google_service_account.gke_node_sa.email}"
 }
 
-# Grant the service account permissions on itself
+# Grant the service account permissions to resource manager service account
 resource "google_service_account_iam_binding" "gke_node_sa_user" {
   service_account_id = google_service_account.gke_node_sa.name
   role               = "roles/iam.serviceAccountUser"
--- erflo-on-jikei/modules/gke_node_sa/variables.tf
+++ erflo-on-jikei/modules/gke_node_sa/variables.tf
@@ -3,6 +3,17 @@ variable "cluster_name" {
   type        = string
 }
 
+variable "gke_node_sa_roles" {
+  description = "The GKE node service account roles"
+  type        = list(string)
+  default     = [
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/monitoring.viewer",
+    "roles/stackdriver.resourceMetadata.writer"
+  ]
+}
+
 variable "project_id" {
   description = "The project ID to host the cluster in"
   type        = string
